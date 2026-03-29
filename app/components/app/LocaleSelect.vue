510aca2d09c650481577d868db1ca725effb70b8
Silas <silas.dew@outlook.com>
2026-03-29 23:19:37 +0800

    feat: 新增语言选择组件，重构导航组件以移除语言选择逻辑

---
+++ app/components/app/LocaleSelect.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { Locale } from '@nuxt/ui'
+import type { LocaleObject } from '@nuxtjs/i18n'
+
+const { locale, locales: localesList, setLocale } = useI18n()
+
+const locales = computed(() => {
+  return localesList.value.map((locale) => ({
+    code: locale.code,
+    name: locale.name
+  })) as Locale<any>[]
+})
+</script>
+
+<template>
+  <ULocaleSelect
+    :model-value="locale"
+    :locales="locales"
+    @update:model-value="(value) => setLocale(value as LocaleObject['code'])"
+    class="w-48"
+  />
+</template>
--- app/components/app/Nav.vue
+++ app/components/app/Nav.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-import type { Locale } from '@nuxt/ui'
-import type { LocaleObject } from '@nuxtjs/i18n'
-
-const { locale, locales: localesList, setLocale } = useI18n()
 const localePath = useLocalePath()
 const { nav } = useNav()
 const colorMode = useColorMode()
@@ -10,22 +6,9 @@ const colorMode = useColorMode()
 const toggleColorMode = () => {
   colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
 }
-
-const locales = computed(() => {
-  return localesList.value.map((locale) => ({
-    code: locale.code,
-    name: locale.name
-  })) as Locale<any>[]
-})
 </script>
 <template>
   <nav class="flex items-center gap-8 text-lg">
-    <ULocaleSelect
-      :model-value="locale"
-      :locales="locales"
-      @update:model-value="(value) => setLocale(value as LocaleObject['code'])"
-      class="w-48"
-    />
     <template v-for="item in nav" :key="item.path">
       <NuxtLink :to="localePath(item.path)" class="cursor-pointer">{{ item.label }}</NuxtLink>
     </template>
