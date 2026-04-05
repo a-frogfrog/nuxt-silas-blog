3cddbf1badf070d46c33d94f17859cee5ca898ab
Silas <silas.dew@outlook.com>
2026-04-05 23:19:40 +0800

    feat: 添加 logo 引用和鼠标事件监听以实现动画效果

--- app/components/Logo.vue
+++ app/components/Logo.vue
@@ -1,10 +1,31 @@
 <script setup lang="ts">
+import { gsap } from 'gsap'
+import { useEventListener } from '@vueuse/core'
+
 const { t } = useI18n()
 const localePath = useLocalePath()
 // 确保服务端/客户端路径一致
 const logoSrc = computed(() => {
   return `/avatar.png`
 })
+
+const logoRef = useTemplateRef('logoRef')
+
+// onMounted(() => {
+//   useEventListener(logoRef.value, 'mouseenter', () => {
+//     gsap.to(logoRef.value, {
+//       duration: 0.5,
+//       skewY: 90,
+//       // skewX: -45
+//     })
+//   })
+//   useEventListener(logoRef.value, 'mouseleave', () => {
+//     gsap.to(logoRef.value, {
+//       duration: 0.5,
+//       skewY: 0
+//     })
+//   })
+// })
 </script>
 
 <template>
@@ -12,6 +33,7 @@ const logoSrc = computed(() => {
     <div class="flex items-center">
       <img
         class="h-20 w-20 rounded-full"
+        ref="logoRef"
         :src="logoSrc"
         alt=""
         aria-hidden="true"
